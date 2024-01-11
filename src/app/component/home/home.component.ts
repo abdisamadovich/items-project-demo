8089bd4542cbedb2a2354f723be4c5ce5456b6e5
abdisamadovich <islombobokulov143@gmail.com>
2024-01-11 15:40:25 +0500

    modalVisible create

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
 export class HomeComponent {
+  public modalVisible: boolean = false;
 
+  public showModal(): void {
+    this.modalVisible = true;
+  }
+
+  public hideModal(): void {
+    this.modalVisible = false;
+  }
+
+  public saveChanges(): void {
+    // do something
+    this.modalVisible = false;
+  }
 }
