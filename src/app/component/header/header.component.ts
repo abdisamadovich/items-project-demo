2182e5164047dfd27eacce81abba114565f46635
abdisamadovich <islombobokulov143@gmail.com>
2024-01-18 19:04:24 +0500

    LogOut qildim

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { Route, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
-export class HeaderComponent {
+export class HeaderComponent{
+  private router: Router = inject(Router)
 
+  public modalLogOutVisible: boolean = false;
+
+  public showLogOutModal(): void{
+    this.modalLogOutVisible = true;
+  }
+
+  public hideLogOutModal(): void{
+    this.modalLogOutVisible = false;
+  }
+
+  public saveLogOutChanges(): void{
+    localStorage.removeItem('access_token');
+    this.modalLogOutVisible = false;
+  }
 }
